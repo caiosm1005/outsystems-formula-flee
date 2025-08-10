9e942cf62f83c95ef681d5aa07998be7b8443009
Caio Santana Magalhães <caiosm1005@gmail.com>
2025-08-10 00:26:57 -0300

    Split CalcEngine's Exceptions.cs into separate class files

--- src/Flee/CalcEngine/PublicTypes/CircularReferenceException.cs
+++ src/Flee/CalcEngine/PublicTypes/CircularReferenceException.cs
@@ -1,8 +1,5 @@
-﻿using Flee.PublicTypes;
-
 namespace Flee.CalcEngine.PublicTypes
 {
-
     public class CircularReferenceException : System.Exception
     {
         private readonly string _myCircularReferenceSource;
@@ -31,24 +28,4 @@ namespace Flee.CalcEngine.PublicTypes
             }
         }
     }
-
-    public class BatchLoadCompileException : Exception
-    {
-
-        private readonly string _myAtomName;
-
-        private readonly string _myExpressionText;
-        internal BatchLoadCompileException(string atomName, string expressionText, ExpressionCompileException innerException) : base(
-            $"Batch Load: The expression for atom '${atomName}' could not be compiled", innerException)
-        {
-            _myAtomName = atomName;
-            _myExpressionText = expressionText;
-        }
-
-        public string AtomName => _myAtomName;
-
-        public string ExpressionText => _myExpressionText;
-    }
-
 }
-
