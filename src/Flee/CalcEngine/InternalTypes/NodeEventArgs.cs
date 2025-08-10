818457059bce5aac666ca7d12cd246967e68b34c
Caio Santana Magalhães <caiosm1005@gmail.com>
2025-08-10 00:24:13 -0300

    Split Miscellaneous.cs into separate class files

---
+++ src/Flee/CalcEngine/InternalTypes/NodeEventArgs.cs
@@ -0,0 +1,24 @@
+﻿namespace Flee.CalcEngine.InternalTypes
+{
+    public sealed class NodeEventArgs : EventArgs
+    {
+
+        private string _myName;
+
+        private object _myResult;
+
+        internal NodeEventArgs()
+        {
+        }
+
+        internal void SetData(string name, object result)
+        {
+            _myName = name;
+            _myResult = result;
+        }
+
+        public string Name => _myName;
+
+        public object Result => _myResult;
+    }
+}
